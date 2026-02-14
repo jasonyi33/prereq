eee9a5cd0904b5b8081681aa9e7da32c1cfcffbf
shay <shay.manor@gmail.com>
2026-02-14 01:46:03 -0800

    feat(api): students routes with CRUD operations

--- api/app.py
+++ api/app.py
@@ -3,10 +3,14 @@ import os
 from flask import Flask
 from flask_cors import CORS
 
+from api.src.routes.courses import courses
+from api.src.routes.students import students
 from src.routes.create import create
 
 app = Flask(__name__, static_folder="", static_url_path="")
 app.register_blueprint(create)
+app.register_blueprint(courses)
+app.register_blueprint(students)
 CORS(app)
 
 
